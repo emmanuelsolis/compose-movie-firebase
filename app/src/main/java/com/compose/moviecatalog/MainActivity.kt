c2a03213f4cea484bb24afb5ee31efaff5fc973b
Emmanuel Solis <emma.ruso@outlook.com>
2024-11-22 23:59:07 -0600

    Logramos guardar y recuperar documentos desde la consola

--- app/src/main/java/com/compose/moviecatalog/MainActivity.kt
+++ app/src/main/java/com/compose/moviecatalog/MainActivity.kt
@@ -1,47 +1,71 @@
 package com.compose.moviecatalog
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.compose.moviecatalog.ui.theme.MovieCatalogTheme
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+
 
 class MainActivity : ComponentActivity() {
+    val  db = FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MovieCatalogTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    addCity("Colima", 100000)
+                    ShowCities(db)
                 }
             }
         }
     }
-}
+
+
+
+
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun addCity(cityName: String, inhabitants: Int) {
+    val db = FirebaseFirestore.getInstance()
+    val city = hashMapOf(
+        "nombre" to cityName,
+        "habitantes" to inhabitants
     )
+    db.collection("ciudades")
+        .add(city)
+        .addOnSuccessListener { documentReference ->
+            Log.d("Firebase", "Documento agregado con ID: ${documentReference.id}")
+        }
+        .addOnFailureListener { e ->
+            Log.e("Firebase", "Error al agregar documento", e)
+        }
 }
-
-@Preview(showBackground = true)
+// Función composable que muestra la lista de ciudades
 @Composable
-fun GreetingPreview() {
-    MovieCatalogTheme {
-        Greeting("Android")
-    }
+fun ShowCities(db: FirebaseFirestore) {
+    val  db = FirebaseFirestore.getInstance()
+    db.collection("ciudades").get()
+        .addOnSuccessListener { result ->
+            for (document in result) {
+                document?.let {
+                    Log.d("Firebase", "DocumentSnapshot data: ${document.data}")
+                    val nombre = document.getString("nombre")
+                    val habitantes = document.getLong("habitantes")
+                    Log.d("Firebase", "Nombre: $nombre")
+                    Log.d("Firebase", "Habitantes: $habitantes")
+                }
+            }
+        }
+        .addOnFailureListener { error ->
+            Log.e("Firebase", "Error al obtener documentos", error)
+        }
 }
